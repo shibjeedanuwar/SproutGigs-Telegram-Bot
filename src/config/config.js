9efa71a78c24b67f53d1c853b115281ee6aba48d
Shibjee Danuwar <120221023+shibjeedanuwar@users.noreply.github.com>
2025-02-22 12:42:52 +0545

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,14 @@
-require('dotenv').config(); 
+require('dotenv').config();
 
 const TOKEN = process.env.BOT_TOKEN;
 const userId = process.env.USER_ID;
 const api_secret = process.env.API_SECRET;
+
+// Error handling for missing environment variables
+if (!TOKEN || !userId || !api_secret) {
+  console.error("Missing required environment variables!");
+  process.exit(1);
+}
 
 const authString = `${userId}:${api_secret}`;
 const auth = Buffer.from(authString).toString('base64');
