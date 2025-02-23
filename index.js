8aac8afb765ca4827c7548ffdacfd673d4213be2
Shibjee Danuwar <120221023+shibjeedanuwar@users.noreply.github.com>
2025-02-23 09:52:25 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const TelegramBot = require('node-telegram-bot-api');
 const config = require('./src/config/config');
 const commandHandlers = require('./src/commands/handlers');
 const { handleCallbackQuery } = require('./src/commands/handleCallbackQuery');
-const express = require('express');
-const app = express();
-// const PORT = 8080;
-
-const userStates = new Map();
-
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(config.TOKEN, { polling: true });
+
+const userStates = new Map();
 
 // Initialize command handlers
 commandHandlers.init(bot, userStates);
@@ -18,13 +14,11 @@ commandHandlers.init(bot, userStates);
 // Update callback query handler
 bot.on('callback_query', async (callbackQuery) => {
     try {
-      await handleCallbackQuery(bot, callbackQuery, userStates);
+        await handleCallbackQuery(bot, callbackQuery, userStates);
     } catch (error) {
-      console.error("Error handling callback query:", error);
+        console.error("Error handling callback query:", error);
     }
 });
 
-//listen express server.
-// app.listen(PORT, () => {
-//   console.log(`Server http://localhost:${PORT} running`);
-// });
+// Optionally, log that the bot is running
+console.log('Telegram bot is running...');
